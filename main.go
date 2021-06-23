7d2c9befcdccc34b82cc898794afbcff116962d3
Oyinda David <oyindadavid18@gmail.com>
2021-06-23 12:56:31 +0100

    added channel to store the values from the goroutine

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 )
 
 var apis map[int]string
+var c chan map[int]interface{}
 
 func getDataResponse(API int) {
 	url := apis[API]
@@ -17,20 +18,26 @@ func getDataResponse(API int) {
 		if body, err := ioutil.ReadAll(resp.Body); err == nil {
 			var result map[string]interface{}
 			json.Unmarshal([]byte(body), &result)
+			var re = make(map[int]interface{})
 			switch API {
 
 			case 1:
 				if result["success"] == true {
-					fmt.Println(result["rates"].(map[string]interface{})["USD"])
+					re[API] = result["rates"].(map[string]interface{})["USD"]
 				} else {
-					fmt.Println(result["error"].(map[string]interface{})["info"])
+					re[API] = result["rates"].(map[string]interface{})["info"]
 				}
+				// store the result into the channel
+				c <- re
+				fmt.Println("Result for API 1 stored")
 			case 2: // for the openweathermap.org API
 				if result["main"] != nil {
-					fmt.Println(result["main"].(map[string]interface{})["temp"])
+					re[API] = result["main"].(map[string]interface{})["temp"]
 				} else {
-					fmt.Println(result["message"])
+					re[API] = result["message"]
 				}
+				c <- re
+				fmt.Println("Result for API 2 stored")
 
 			}
 		} else {
@@ -42,6 +49,10 @@ func getDataResponse(API int) {
 }
 
 func main() {
+	// creates a channel to store the results from the
+	// API calls
+
+	c = make(chan map[int]interface{})
 	apis = make(map[int]string)
 	apis[1] = "http://data.fixer.io/api/latest?access_key=21ce410f9900f7c07838c2e6d1f80e31"
 	apis[2] = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=61b3761a8c87df93dff55c1ca9eb93a4"
@@ -49,5 +60,11 @@ func main() {
 	go getDataResponse(1)
 	go getDataResponse(2)
 
+	// we expect two results in the channel
+	for i := 0; i < 2; i++ {
+		fmt.Println(<-c)
+	}
+
+	fmt.Println("Done!")
 	fmt.Scanln()
 }
